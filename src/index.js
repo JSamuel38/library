7d41372c37fd82a4498e887cbf95b0db8416e9c2
Samuel Evans <117552758+JSamuel38@users.noreply.github.com>
2023-01-20 18:58:02 +0000

    Add method to display book information to page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 let myLibrary = [];
 
+const bookContainer = document.querySelector('.books-container');
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -11,3 +13,15 @@ Book.prototype.info = function () {
   (this.read) ? this.read = 'has been read' : this.read = 'not read yet';
   return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
 };
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
+function displayBooks(library) {
+  library.forEach((book) => {
+    const card = document.createElement('div');
+    card.innerText = book.info();
+    bookContainer.appendChild(card);
+  });
+}
